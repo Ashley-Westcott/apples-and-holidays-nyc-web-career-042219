a12577767f0ae166e32547cdf90553ceacb29c79
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-17 03:53:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do | season, holiday |
-    holiday.collect do | event, supplies |
+    holiday.map do | event, supplies |
       if supplies.include? 'BBQ'
       puts supplies
       else N/A
